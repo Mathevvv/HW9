b634330e613928af8e1f02762631f15a5e5b9950
matve <matvey.karasev.00@gmail.com>
2024-11-24 13:27:29 +0300

    task2 was completed

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
+        System.out.println(analiz(vvod()));
+    }
+    private static int vvod() {
+        System.out.println("Введите целое число");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    private static boolean analiz(int q) {
+        if (q%2==0) return true;
+        return false;
     }
 }
