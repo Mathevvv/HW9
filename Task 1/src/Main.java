8aa49f8700ce2d770cbd4f772b5ded2a7e9a8a30
matve <matvey.karasev.00@gmail.com>
2024-11-24 12:49:41 +0300

    task1 was completed

--- Task 1/src/Main.java
+++ Task 1/src/Main.java
@@ -1,36 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     static double a = 0;
     static double b = 0;
 
     public static void main(String[] args) {
+        System.out.println("Введите 2 числа");
         //todo Вам нужно дополнить методы так, чтобы получился рабочий калькулятор
         a = inputDouble();
         b = inputDouble();
 
-        sum();      //Метод суммирования чисел
-        multiply(); //Метод умножения чисел
-        diff();     //Метод вычитания чисел
-        divide();   //Метод деления чисел
+        sum(a,b);      //Метод суммирования чисел
+        multiply(a,b); //Метод умножения чисел
+        diff(a,b);     //Метод вычитания чисел
+        divide(a,b);   //Метод деления чисел
     }
 
-    private static void divide() {
+    private static void divide(double q, double w) {
         //todo дописать логику и печать результата
+        System.out.println("a/b =" + q/w);
     }
 
-    private static void diff() {
+    private static void diff(double q, double w) {
         //todo дописать логику и печать результата
+        System.out.println("a-b =" + (q-w));
     }
 
-    private static void multiply() {
+    private static void multiply(double q, double w) {
         //todo дописать логику и печать результата
+        System.out.println("a*b =" + q*w);
     }
 
-    private static void sum() {
+    private static void sum(double q, double w) {
         //todo дописать логику и печать результата
+        System.out.println("a+b =" + (q+w));
     }
 
     private static double inputDouble() {
         //todo дописать логику
-        return 0;
+        Scanner sc = new Scanner(System.in);
+        return sc.nextDouble();
     }
 }
